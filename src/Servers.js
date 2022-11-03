ab1b0b99aab0567ea18938c2dc82906b14366435
Cameron West <camonater62@gmail.com>
2022-11-03 12:37:17 -0700

    independent channels

--- src/Servers.js
+++ src/Servers.js
@@ -8,28 +8,20 @@
 // - Change the users to not be so hard coded
 const smallFriendServer = new Server("imgs/server-icons/smallfriendserver.png", "Me and my buds :)");
 smallFriendServer.users = Userlist(7); //add 7 users
-// empty text is just here for testing, we can delete later
 smallFriendServer.textchannels = [
     new TextChannel("# general"),
     new TextChannel("# empty text"),
 ];
-
-console.log(smallFriendServer.users.length); //testing code, plz save it for now
 smallFriendServer.voicechannels = Vclist(1,smallFriendServer.users);//add 2 voice channels with random members
-for(var i = 0; i<smallFriendServer.voicechannels[0].currentUsers.length; i++){//testing code, prints out vc members
-    console.log(smallFriendServer.voicechannels[0].currentUsers[i].name);
-}
-//Voiceuser(smallFriendServer.users);
 
-// testing server, please keep for now
+
 const bigFriendServer = new Server("imgs/server-icons/bigfriendserver.png", "2 buds 2 furious");
 bigFriendServer.users = Userlist(15);
 bigFriendServer.textchannels = [
     new TextChannel("This is a different text channel"),
 ]
-bigFriendServer.voicechannels = bigFriendServer.voicechannels = Vclist(1,bigFriendServer.users);
+bigFriendServer.voicechannels = Vclist(1,bigFriendServer.users);
 
-// another testing server
 const classServer = new Server("imgs/server-icons/classserver.png", "Class Server");
 classServer.textchannels = [
     new TextChannel("This is a different text channel"),
@@ -37,7 +29,7 @@ classServer.textchannels = [
 classServer.voicechannels = [
     new VoiceChannel("This is another different voice channel"),
 ]
-classServer.users = Userlist(2);
+classServer.users = Userlist(0);
 
 function Userlist(num){           // generate random userlist for new server, num = numbers of users
     names=[];
